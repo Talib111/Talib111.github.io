74eeae4c080d1101e237a45c65461f5adb689581
Talib111 <talibplay111@gmail.com>
2023-05-21 01:36:53 +0530

    changes

--- assets/preview-1049c9f6.js
+++
@@ -1,2 +0,0 @@
-const e={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}},canvas:{hidden:!0}};export{e as parameters};
-//# sourceMappingURL=preview-1049c9f6.js.map
